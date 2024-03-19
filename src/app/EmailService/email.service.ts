285363b6c0784fb3b495907bcc448cdb59d7c1f6
Gabriel Perlaza <gabrieldeberes@gmail.com>
2024-03-18 23:37:57 -0500

    Se configuro el contact para enviar peticion HTTP POST por medio del url localhost y los parametros, la aplicacion no conecta con back-end

--- src/app/EmailService/email.service.ts
+++ src/app/EmailService/email.service.ts
@@ -16,7 +16,7 @@ export class EmailService {
 
     let body = 'data='+JSON.stringify(data);
 
-   return this.http.post('https://drive.google.com/file/d/1BaLmQ345cOkk0jQTun2D3iZAdh5tTtQl/view?usp=sharing', body, {headers: headers});
+   return this.http.post('', body, {headers: headers});
   }
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DatosService } from '../Datos-Service/datos.service';
 import { EmailService } from '../EmailService/email.service';
 import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -11,29 +11,33 @@ import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms'
 })
 export class ContactComponent {
 
-  constructor(private emailService: EmailService){}
+  datos : FormGroup;
+  constructor(private http: HttpClient){
+    this.datos = new FormGroup({
+      "nombre" : new FormControl('', Validators.required),
+      "correo" : new FormControl('', [Validators.required, Validators.email]),
+      "mensaje" : new FormControl('', Validators.required)
+    })
+  }
 
   ngOnInit() : void {}
 
- 
-
-    public formBuild = {
-      "nombre" : new FormControl(''),
-      "correo" : new FormControl(''),
-      "mensaje" : new FormControl('')
+    envioCorreo(){
+      let params = {
+        nombre : this.datos.value.nombre,
+        correo : this.datos.value.correo,
+        mensaje : this.datos.value.mensaje
+      }
+      console.log(params);
+      this.http.post('http://localhost:3000/envio', params).subscribe(resp =>{
+        console.log(resp);
+      });
     }
 
-    sendEmail(){
-      try{
-        this.emailService.sendEmail(this.formBuild).subscribe();
-      }
-      catch(error){
-        console.log(error);
-      }
-      
-    }
+  
 
-  }
+}
+
 
 
   
